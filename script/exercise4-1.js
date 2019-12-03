ac525b99e4d499aaa4a8a2a2f5f36e04e7082820
sara <sarahuangg7@gmail.com>
2019-12-03 18:02:59 -0500

    Create exercise4-1.js

---
+++ script/exercise4-1.js
@@ -0,0 +1,35 @@
+$('#btn1').click(function() {
+	console.log("btnl is clicked!");
+});
+$( function() {
+    $( '#target1' ).draggable();
+  } );
+
+$( function() {
+    $( '#btn1' ).draggable();
+  } );
+
+
+$('.face').click(function() {
+	console.log("btnl is clicked!");
+});
+$( function() {
+    $( '.face' ).draggable();
+  } );
+$( function() {
+    $( '.left_eye' ).draggable();
+  } );
+$( function() {
+    $( '.right_eye' ).draggable();
+  } );
+$( function() {
+    $( '.nose' ).draggable();
+  } );
+$( function() {
+    $( '.mouth' ).draggable();
+  } );
+
+$( function() {
+    $( ".face" ).selectable();
+  } );
+
